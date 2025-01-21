9ea648700a4cca5624f47955fef778c766a43fd4
elbendir <elmelbendir@gmail.com>
2025-01-21 12:13:45 +0100

    Analyse Cadorim

--- cado/cado/urls.py
+++ cado/cado/urls.py
@@ -18,14 +18,9 @@ from django.contrib import admin
 from django.urls import path, include  # Utilisez 'include' pour inclure les URLs de l'application rim
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rim.urls')),
     path('rim/', include('rim.urls')),  # Inclure les URLs de l'application 'rim'
 ]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
-
-   
-
-urlpatterns += staticfiles_urlpatterns()
